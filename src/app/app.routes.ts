64526d4bdc086ea11d09fb64c65f36b1f5adf311
mudit023 <muditmishra023@gmail.com>
2024-09-30 23:39:23 +0530

    fcuked

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-    {
-        path:'landing',
-        loadChildren: () => import('./landing-page/landing-page.routes').then(m=>m.landingPageRoutes)
-    }
-];
--- src/app/landing-page/landing-page.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import { Routes } from '@angular/router';
-import { LandingPageComponent } from './landing-page.component';
-
-export const landingPageRoutes: Routes = [
-  {
-    path: '',component:LandingPageComponent}
-];
