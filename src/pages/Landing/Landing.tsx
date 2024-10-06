105cc1f1a514d5180807fd1ff5db3ca510716b4c
mudit023 <muditmishra023@gmail.com>
2024-10-07 02:56:49 +0530

    landing design changes

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -8,9 +8,9 @@ function Landing() {
         <header className={styles["landing-header"]}>
           <div className={styles["landing-logo"]}>
             <span>Job</span>
-                <span className="material-symbols-outlined">location_on</span>
-              <br />
-              <span>Tracker</span>
+            <span className="material-symbols-outlined">location_on</span>
+            <br />
+            <span>Tracker</span>
           </div>
           <div className={styles["landing-nav"]}>
             <button className={styles["nav-link"]}>Signup</button>
@@ -19,22 +19,24 @@ function Landing() {
         <div className={styles["landing-content"]}>
           <div className={styles["content-details"]}>
             <div className={styles["title-container"]}>
-              <h3 className={styles["content-sub_title"]}>
-                Lorem ipsum dolor
-              </h3>
+              <h3 className={styles["content-sub_title"]}>Lorem ipsum dolor</h3>
               <h1 className={styles["content-title"]}>
-                Track To <span className={styles["content-title-span"]}>Win</span>
+                Track To{" "}
+                <span className={styles["content-title-span"]}>Win</span>
               </h1>
               <p className={styles["content-description"]}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </p>
             </div>
             <div className={styles["cta-btn-container"]}>
-              <button className={styles["cta-btn"]}>Track Now</button>
+              <button className={styles["cta-btn"]}>
+                <span>Track Now</span>
+                <span className="material-symbols-outlined">trending_flat</span>
+              </button>
             </div>
           </div>
           <div className={styles["content-image"]}>
-            <img src={HeroSvg} alt="image"/>
+            <img src={HeroSvg} alt="image" />
           </div>
         </div>
       </section>
